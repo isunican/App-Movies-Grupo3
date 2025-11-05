7a3cc80261a3830cfe330a9201b1ae6776ffa674
javisiierra <jsm422@alumnos.unican.es>
2025-11-05 17:26:46 +0100

    Mover el drawmatcher a matchers

--- AndroidProject/app/src/androidTest/java/es/unican/movies/utils/Matchers.java
+++ AndroidProject/app/src/androidTest/java/es/unican/movies/utils/Matchers.java
@@ -1,16 +1,45 @@
 package es.unican.movies.utils;
 
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+
+import java.util.Objects;
 
 /**
  * Custom matchers for UI tests.
  */
 public class Matchers {
 
-    // Include custom matchers here
+    public static Matcher<View> hasDrawable(int drawableResId) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof ImageButton)) {
+                    return false;
+                }
+                ImageButton imageButton = (ImageButton) view;
+                Drawable expectedDrawable = ContextCompat.getDrawable(view.getContext(), drawableResId);
+                Drawable actualDrawable = imageButton.getDrawable();
+
+                if (expectedDrawable == null || actualDrawable == null) {
+                    return false;
+                }
+
+                return Objects.equals(expectedDrawable.getConstantState(), actualDrawable.getConstantState());
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has drawable with resource id: " + drawableResId);
+            }
+        };
+    }
 
 }
